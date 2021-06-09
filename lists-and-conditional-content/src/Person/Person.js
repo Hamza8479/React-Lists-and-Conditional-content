c60182b9fb29aecd24bef6a66d07044232cb7a4c
Hamza <hamzazaman500@gmail.com>
2021-06-09 11:47:04 +0500

    working with css module

--- lists-and-conditional-content/src/Person/Person.js
+++ lists-and-conditional-content/src/Person/Person.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import classes from "./Person.module.css";
 
 function Person(props) {
   // Radium
@@ -11,20 +12,20 @@ function Person(props) {
   // };
 
   // Styled component
-  const StyledDiv = styled.div`
-    width: 60%;
-    margin: 16px auto;
-    border: 1px solid #eee;
-    box-shadow: 0px 2px 3px #ccc;
-    padding: 16px;
-    text-align: center;
-    @media (min-width: 400px) {
-      width: 400px;
-    }
-  `;
+  // const StyledDiv = styled.div`
+  //   width: 60%;
+  //   margin: 16px auto;
+  //   border: 1px solid #eee;
+  //   box-shadow: 0px 2px 3px #ccc;
+  //   padding: 16px;
+  //   text-align: center;
+  //   @media (min-width: 400px) {
+  //     width: 400px;
+  //   }
+  // `;
   return (
-    // <div className="Person" style={style}>
-    <StyledDiv>
+    <div className={classes.Person}>
+      {/* //  <StyledDiv> */}
       <p onClick={props.click}>
         {" "}
         I'm {props.name} and I'm {props.age} years old{" "}
@@ -37,7 +38,8 @@ function Person(props) {
         onChange={props.changed}
         value={props.name}
       />
-    </StyledDiv>
+      {/* </StyledDiv> */}
+    </div>
   );
 }
 
