58a8cfb69c43b99dc12f051e7f0300f5600a93f7
Hamza <hamzazaman500@gmail.com>
2021-06-16 11:10:00 +0500

    Fragment

--- lists-and-conditional-content/src/components/Persons/Persons.js
+++ lists-and-conditional-content/src/components/Persons/Persons.js
@@ -1,20 +1,22 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Person from "./Person/Person";
 
 const Persons = (props) =>
   props.persons.map((prsn, index) => {
     return (
-      <Person
-        name={prsn.name}
-        age={prsn.age}
-        click={() => {
-          props.clicked(index);
-        }}
-        key={prsn.id}
-        changed={(event) => {
-          props.changedname(event, prsn.id);
-        }}
-      />
+      <Fragment>
+        <Person
+          name={prsn.name}
+          age={prsn.age}
+          click={() => {
+            props.clicked(index);
+          }}
+          key={prsn.id}
+          changed={(event) => {
+            props.changedname(event, prsn.id);
+          }}
+        />
+      </Fragment>
     );
   });
 
